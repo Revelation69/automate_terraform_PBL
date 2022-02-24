dd690f2af2f1a2647e158ee9be3e5a8b98023012
Taiwo Lawal <taiwolawal360@gmail.com>
2022-02-24 16:20:56 +0100

    init working

--- providers.tf
+++ providers.tf
@@ -2,12 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      #version = "~> 4.0"
+      version = "~> 4.0"
     }
   }
 }
 
 provider "aws" {
   region = var.region
+  
 }
 
