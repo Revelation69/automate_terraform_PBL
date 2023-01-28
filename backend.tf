5b2134a134ffb6b02c622bed93195d6a16a67a51
Michael Ajala <michaeloajala@gmail.com>
2023-01-28 23:59:49 +0100

    made changes

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "mike-pbl18"
-#     key            = "global/s3/terraform.tfstate"
-#     region         = "us-west-2"
-#     dynamodb_table = "terraform-locks"
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "mike-pbl18"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
 
 terraform {
   backend "remote" {
