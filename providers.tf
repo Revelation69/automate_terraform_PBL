3bfadfc9938e38b0799580472aa0ea7d3d5549ac
Michael Ajala <michaeloajala@gmail.com>
2023-01-28 16:12:08 +0100

    made changes

--- providers.tf
+++ providers.tf
@@ -2,11 +2,13 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-2"
+  region = var.region
 }
