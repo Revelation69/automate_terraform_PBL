fee673f0ab09af3089c18e29122ad604110cfd51
Michael Ajala <michaeloajala@gmail.com>
2023-01-28 15:41:47 +0100

    made changes

--- main.tf
+++ main.tf
@@ -7,13 +7,6 @@ resource "aws_s3_bucket_versioning" "terraform_state" {
   enabled = true
   force_destroy = true
 }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-locks"
