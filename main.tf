282797e1f4c6b83bb1ff27264f3ee25ecf05e75a
Michael Ajala <michaeloajala@gmail.com>
2023-02-02 11:16:58 +0100

    made changes

--- main.tf
+++ main.tf
@@ -43,6 +43,7 @@ module "VPC" {
   source                              = "./modules/VPC"
   region                              = var.region
   vpc_cidr                            = var.vpc_cidr
+  enable_dns_support                  = var.enable_dns_support
   enable_dns_hostnames                = var.enable_dns_hostnames
   preferred_number_of_public_subnets  = var.preferred_number_of_public_subnets
   preferred_number_of_private_subnets = var.preferred_number_of_private_subnets
